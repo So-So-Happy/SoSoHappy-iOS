f938291589f6adfe8fd64e9a3539fdd87534959b
suekim999 <suekim999@naver.com>
2023-08-01 23:16:35 +0900

    [Add] 프로젝트 파일 추가

---
+++ SoSoHappy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoSoHappy
+//
+//  Created by Sue on 2023/08/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
