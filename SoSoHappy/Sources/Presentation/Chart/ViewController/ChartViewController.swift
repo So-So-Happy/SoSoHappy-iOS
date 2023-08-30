6557b64d7951d5f71727a1a51618335b31b97b30
Minju Park <mlnjv016@gmail.com>
2023-08-30 15:51:47 +0900

    [Refactor] ChartVC 뷰 분리 및 코드 리팩토링 (#2)

--- SoSoHappy/Sources/Presentation/Chart/ViewController/ChartViewController.swift
+++ SoSoHappy/Sources/Presentation/Chart/ViewController/ChartViewController.swift
@@ -6,26 +6,44 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class ChartViewController: UIViewController {
-    let chartView = ChartView()
+final class ChartViewController: UIViewController {
+    
+    private lazy var awardsView = AwardsView()
+    private lazy var recommendView = RecommendView()
+    private lazy var chartView = ChartView()
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "BGgrayColor")
         
-        setUpUI()
-        
+        setUpView()
     }
     
-    func setUpUI() {
-        // Add the custom view to the view controller's view
+    private func setUpView() {
+        view.addSubview(awardsView)
+        view.addSubview(recommendView)
         view.addSubview(chartView)
         
-        // Set up constraints using SnapKit to make the custom view fill the screen
+        awardsView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(310) // 이 부분은 awardsView의 높이 계산에 맞게 변경해야 함
+            
+        }
+
+        recommendView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(awardsView.snp.bottom)
+            make.height.equalTo(130)
+        }
+
         chartView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.leading.trailing.equalToSuperview()
+            make.top.equalTo(recommendView.snp.bottom)
         }
     }
 }
