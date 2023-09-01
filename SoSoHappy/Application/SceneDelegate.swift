451ee6547aecf9abf3e45f6901ddc50ca24f6e00
Wonder Park <pkkyung26@gmail.com>
2023-09-01 19:51:46 +0900

    [Chore] SceneDelegate 파일에 Coordinator 추가

--- SoSoHappy/Application/SceneDelegate.swift
+++ SoSoHappy/Application/SceneDelegate.swift
@@ -10,21 +10,26 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var coordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let mainVC = UINavigationController(rootViewController: AddStep1ViewController())
-//        window.rootViewController = mainVC // 시작 VC 작성해주기
-        window.rootViewController = CalendarViewController() // 시작 VC 작성해주기
-        window.makeKeyAndVisible()
         self.window = window
+        
+//        let mainVC = UINavigationController(rootViewController: AddStep1ViewController())
+//        window.rootViewController = mainVC // 시작 VC 작성해주기
+        
+        let navigationController = UINavigationController()
+        window.rootViewController = navigationController
+        
+        coordinator = AppCoordinator(navigationController: navigationController)
+        coordinator?.start()
+        
+        window.rootViewController = navigationController
+        
+        window.makeKeyAndVisible()
     }
     
     func sceneDidDisconnect(_ scene: UIScene) {
