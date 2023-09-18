58385f36bcf38b8c59ac6be23904d315f41f59b3
suekim999 <suekim999@naver.com>
2023-09-18 18:58:55 +0900

    [Feat] OwnerFeedCell Reactor 구현 및 Cell 과 연결 구현 (#23)

---
+++ SoSoHappy/Sources/Presentation/Feed/Reactor/OwnerFeedCellReactor.swift
@@ -0,0 +1,52 @@
+//
+//  OwnerFeedCellReactor.swift
+//  SoSoHappy
+//
+//  Created by Sue on 2023/09/11.
+//
+
+import ReactorKit
+
+class OwnerFeedCellReactor: Reactor {
+    enum Action {
+        case toggleLike
+    }
+    
+    enum Mutation {
+        case setFeed(Feed)
+    }
+    
+    struct State {
+        var feed: Feed?
+    }
+    
+    let initialState: State
+    
+    init(state: State) {
+        self.initialState = state
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .toggleLike:
+            // Toggle the like state and emit a Mutation to update the UI
+            guard var updatedFeed = currentState.feed else { fatalError() }
+            updatedFeed.isLike = !updatedFeed.isLike
+            // update된 피드 서버에도 알려주기
+            return Observable.just(.setFeed(updatedFeed))
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+//        case let .setLike(like):
+//            newState.isLike = like
+        case .setFeed(let feed):
+            newState.feed = feed
+        }
+        
+        return newState
+    }
+}
+
