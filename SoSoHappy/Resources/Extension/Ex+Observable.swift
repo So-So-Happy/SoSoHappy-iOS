79219290a525a1959829c97f5af49ad21daca31d
Wonder Park <pkkyung26@gmail.com>
2023-09-18 19:13:22 +0900

    [Add] Encodable + Observable Extension 파일 추가

---
+++ SoSoHappy/Resources/Extension/Ex+Observable.swift
@@ -0,0 +1,27 @@
+//
+//  Ex+Observable.swift
+//  SoSoHappy
+//
+//  Created by 박희경 on 2023/09/11.
+//
+
+import Foundation
+import RxSwift
+
+
+extension Observable where Element == Data {
+    func map<T: Decodable>(_ type: T.Type) -> Observable<T> {
+        flatMap { element -> Observable<T> in
+            .create { observer in
+                let decoder = JSONDecoder()
+                do {
+                    let model = try decoder.decode(T.self, from: element)
+                    observer.onNext(model)
+                } catch {
+                    observer.onError(NSError())
+                }
+                return Disposables.create()
+            }
+        }
+    }
+}
