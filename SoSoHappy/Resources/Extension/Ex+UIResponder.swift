61390b26909beceb8fd648a016bb5a41bdeb51e1
suekim999 <suekim999@naver.com>
2023-09-18 11:36:35 +0900

    [Feat] Ex+ UIResponder에 현재 responder를 가져오는 함수 구현

---
+++ SoSoHappy/Resources/Extension/Ex+UIResponder.swift
@@ -0,0 +1,27 @@
+//
+//  Ex+UIResponder.swift
+//  SoSoHappy
+//
+//  Created by Sue on 2023/09/04.
+//
+
+import UIKit
+
+extension UIResponder {
+
+    private struct Static {
+        static weak var responder: UIResponder?
+    }
+
+    /// Finds the current first responder
+    /// - Returns: the current UIResponder if it exists
+    static func currentFirst() -> UIResponder? {
+        Static.responder = nil
+        UIApplication.shared.sendAction(#selector(UIResponder._trap), to: nil, from: nil, for: nil)
+        return Static.responder
+    }
+
+    @objc private func _trap() {
+        Static.responder = self
+    }
+}
