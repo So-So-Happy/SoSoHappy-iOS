1c6980badad6ca2de2439665a3ee6f1107ad99d5
suekim999 <suekim999@naver.com>
2023-09-25 14:28:20 +0900

    [Feat] HappyListViewController에 Reactor 연결

--- SoSoHappy/Sources/Presentation/Calender/ViewController/HappyListViewController .swift
+++ SoSoHappy/Sources/Presentation/Calender/ViewController/HappyListViewController .swift
@@ -9,14 +9,20 @@ import UIKit
 import SnapKit
 import RxSwift
 import RxCocoa
+import ReactorKit
 import ImageSlideshow
 
 final class HappyListViewController : UIViewController {
-        
     // MARK: - Properties
+    var disposeBag = DisposeBag()
+    
+    // MARK: - UI Components
     private lazy var happyTableView = UITableView().then {
-        $0.register(HappyListCell.self, forCellReuseIdentifier: HappyListCell.identifier)
+        $0.register(HappyListCell.self, forCellReuseIdentifier: HappyListCell.cellIdentifier)
+        $0.backgroundColor = UIColor(named: "backgroundColor")
         $0.separatorStyle = .none
+        $0.estimatedRowHeight = 30
+        $0.rowHeight = UITableView.automaticDimension
     }
     
     private lazy var yearMonthLabel = UILabel().then {
@@ -25,13 +31,9 @@ final class HappyListViewController : UIViewController {
         $0.textColor = UIColor(rgb: 0x626262)
     }
     
-    var imageSlideView = ImageSlideView()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureUI()
-        happyTableView.delegate = self
-        happyTableView.dataSource = self
+        setLayout()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -42,19 +44,30 @@ final class HappyListViewController : UIViewController {
         super.viewWillDisappear(animated)
     }
     
+    init(reactor: HappyListViewReactor) {
+        super.init(nibName: nil, bundle: nil)
+        self.reactor = reactor
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
 
 // MARK: - Layout & Attribute
 private extension HappyListViewController  {
-    func configureUI() {
+    private func setLayout() {
         self.view.backgroundColor = UIColor(rgb: 0xF5F5F5)
-        self.happyTableView.backgroundColor = UIColor(rgb: 0xF5F5F5)
         self.view.addSubview(self.happyTableView)
         self.view.addSubview(self.yearMonthLabel)
+        
         self.yearMonthLabel.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide).offset(10)
             $0.centerX.equalToSuperview()
         }
+        
         self.happyTableView.snp.makeConstraints {
             $0.bottom.left.right.equalToSuperview()
             $0.top.equalTo(yearMonthLabel.snp.bottom).offset(20)
@@ -62,62 +75,35 @@ private extension HappyListViewController  {
     }
 }
 
-// MARK: - tableView DataSource protocol -> rx.items(datasource )
- extension HappyListViewController : UITableViewDataSource {
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: HappyListCell.identifier, for: indexPath) as? HappyListCell else { return UITableViewCell() }
+// MARK: - ReactorKit - bind func
+extension HappyListViewController: View {
+    // MARK: bind
+    func bind(reactor: HappyListViewReactor) {
+        // MARK: Action (View -> Reactor) 인풋
+        self.rx.viewDidLoad
+            .map { Reactor.Action.fetchRecentSortedFeeds("202307") } // 예시로 넣었음
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
         
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTap(sender:)))
-        cell.imageSlideView.slideShowView.addGestureRecognizer(tapGesture)
-        
-        return cell
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-}
-
-//MARK: - tableView Delegate
-extension HappyListViewController : UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-    }
-}
-
-// MARK: - Action
-extension HappyListViewController {
-    @objc func didTap(sender: UITapGestureRecognizer? = nil) {
-        print("ViewController - didTap() called")
-        let fullScreenController = imageSlideView.slideShowView.presentFullScreenController(from: self)
+        // MARK: State (Reactor -> State) 아웃풋
+        reactor.state
+            .skip(1)
+            .map { $0.feeds }
+            .bind(to: happyTableView.rx.items(cellIdentifier: HappyListCell.cellIdentifier, cellType: HappyListCell.self)) { (row,  feed, cell) in
+                // viewcontroller에서 직접 feed로 데이터를 전달해주는게 적합한지 잘 모르겠음
+                // 이것마저 Reactor에서 다루는 참고 코드가 있는지 확인하고 적용해보면 좋을 듯
+                let cellReactor = HappyListCellReactor(feed: feed)
+                cell.reactor = cellReactor
+                
+                cell.imageSlideView.tapObservable
+                    .subscribe(onNext: { [weak self] in
+                        guard let self = self else { return }
+                        cell.imageSlideView.slideShowView.presentFullScreenController(from: self)
+                    })
+                    .disposed(by: cell.disposeBag)
+                
+            }
+            .disposed(by: disposeBag)
     }
 }
-
-
-#if DEBUG
-import SwiftUI
-struct HappyListViewControllerRepresentable: UIViewControllerRepresentable {
-    
-func updateUIViewController(_ uiView: UIViewController,context: Context) {
-        // leave this empty
-}
-@available(iOS 13.0.0, *)
-func makeUIViewController(context: Context) -> UIViewController {
-    HappyListViewController()
-    }
-}
-@available(iOS 13.0, *)
-struct HappyListViewControllerRepresentable_PreviewProvider: PreviewProvider {
-    static var previews: some View {
-        Group {
-            HappyListViewControllerRepresentable()
-                .ignoresSafeArea()
-                .previewDisplayName("Preview")
-                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
-        }
-        
-    }
-} #endif
-
-
 
