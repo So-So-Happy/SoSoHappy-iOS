f1977598cb9fea2165b14bfcd669703688b7d654
suekim999 <suekim999@naver.com>
2023-09-26 16:08:08 +0900

    [Refactor] ProfileVIew - OwnerFeedHeaderView 상속 받아서 리팩토링 (#35)

--- SoSoHappy/Sources/Presentation/MyPage/View/ProfileVIew.swift
+++ SoSoHappy/Sources/Presentation/MyPage/View/ProfileVIew.swift
@@ -8,132 +8,32 @@
 import UIKit
 import SnapKit
 
-final class ProfileView: UIView {
-    
-    // MARK: - Properties
-    private lazy var profileImage: UIImageView = {
-        // UIImageView 생성
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFill
-        imageView.layer.cornerRadius = imageView.frame.size.width / 2
-        imageView.clipsToBounds = true
-        return imageView
-    }()
-    
-    private lazy var nickNameLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .black
-//        label.font = .boldSystemFont(ofSize: 22)
-        label.font = .systemFont(ofSize: 22, weight: .bold)
-        return label
-    }()
-    
-    private lazy var introLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = UIColor(rgb: 0x6D6D6D)
-        label.font = .systemFont(ofSize: 13)
-        return label
-    }()
-    
-    private lazy var stackView = UIStackView(axis: .vertical, alignment: .center, distribution: .fillEqually, spacing: 5)
-    
-    private lazy var editProfileButton: UIButton = {
-        let button = AutoAddPaddingButtton()
-        button.setTitle("프로필 설정", for: .normal)
-        button.titleLabel?.font = .systemFont(ofSize: 13)
-        //        button.addTarget(, action: , for: )
-        button.frame = CGRect(x: 100, y: 200, width: 220, height: 50)
-        button.backgroundColor = UIColor(rgb: 0xFAAB78)
-        button.setTitleColor(.white, for: .normal)
-        button.layer.cornerRadius = 13
-        button.addTarget(self, action: #selector(test), for: .touchUpInside)
-        
-        return button
-    }()
-    
-    private lazy var dividerLine: UIImageView = {
-        let image = UIImage(named: "dividerLine")
-        let imageView = UIImageView(image: image)
-        return imageView
-    }()
-    
+final class ProfileView: OwnerFeedHeaderView {
+    lazy var profileSetButton = HappyButton().then {
+        $0.setTitle("프로필 설정", for: .normal)
+        $0.titleLabel?.textColor = .white
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+        $0.layer.cornerRadius = 16
+        $0.setBackgroundColor(UIColor.orange, for: .enabled)
+    }
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setup()
+        setProfileView()
     }
     
     required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        setup()
+        fatalError("init(coder:) has not been implemented")
     }
     
-}
-
-extension ProfileView {
-    
-    // MARK: - Layout & Attribute
-    func setup() {
-        setLayout()
-        setAttribute()
-    }
-    
-    func setLayout() {
-        
-        self.addSubviews(profileImage, stackView, editProfileButton, dividerLine)
-        self.stackView.addArrangedSubview(nickNameLabel)
-        self.stackView.addArrangedSubview(introLabel)
-        
-        profileImage.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-            $0.top.equalTo(safeAreaLayoutGuide).inset(40)
-            $0.width.height.equalTo(100)
+    func setProfileView() {
+        stackView.insertArrangedSubview(profileSetButton, at: 3)
+        profileSetButton.snp.makeConstraints { make in
+            make.height.equalTo(36)
+            make.width.equalTo(90)
         }
-        
-        
-        stackView.snp.makeConstraints {
-            $0.top.equalTo(profileImage.snp.bottom).offset(18)
-            $0.centerX.equalToSuperview()
-            $0.height.equalTo(50)
-        }
-        
-        editProfileButton.snp.makeConstraints {
-            $0.centerX.equalToSuperview()
-//            $0.bottom.equalTo(dividerLine.snp.top).offset(-10)
-            $0.top.equalTo(stackView.snp.bottom).offset(13)
-
-        }
-        
-        dividerLine.snp.makeConstraints {
-            $0.horizontalEdges.equalToSuperview()
-            $0.height.equalTo(2)
-            $0.bottom.equalToSuperview()
-        }
-        
-    }
-    
-    func setAttribute() {
-        self.profileImage.image = UIImage(named: "happy2")
-        self.nickNameLabel.text = "소소해"
-        self.introLabel.text = "나는야 소소해. 디저트 러버지"
-    }
-    
-//    @objc func
-    
-    @objc private func test() {
-       
+        profileImageWithBackgroundView.updateProfileImageSize(background: 130, profile: 100)
+        profileNickNameLabel.font = .systemFont(ofSize: 20, weight: .bold)
+        profileSelfIntroduction.font = .systemFont(ofSize: 14, weight: .light)
     }
 }
-
-
-// MARK: - AutoAddPaddingButton 
-class AutoAddPaddingButtton : UIButton
-{
-    override var intrinsicContentSize: CGSize {
-       get {
-           let baseSize = super.intrinsicContentSize
-           return CGSize(width: baseSize.width + 30,//ex: padding 40
-                         height: baseSize.height )
-           }
-    }
-}
-
