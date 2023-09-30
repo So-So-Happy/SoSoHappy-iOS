4c5030000c52cf649e564cd2f38143bc20426b2e
Wonder Park <pkkyung26@gmail.com>
2023-09-30 17:54:12 +0900

    [Add] UpdateLikeRequest, UpdateLikeResponse 구현 (#38)

---
+++ SoSoHappy/Sources/Network/Models/DTO/Request/UpdateLikeRequest.swift
@@ -0,0 +1,23 @@
+//
+//  UpdateLikeRequest.swift
+//  SoSoHappy
+//
+//  Created by 박희경 on 2023/09/30.
+//
+
+import Foundation
+
+
+struct UpdateLikeRequest: Requestable {
+    let srcNickname: String
+    let nickname: String
+    let date: Int64
+    
+    var params: [String : Any] {
+        return [
+            "srcNickname": self.srcNickname,
+            "nickname": self.nickname,
+            "date": self.date
+        ]
+    }
+}
