3a8c81e6bfb42e46090e773027875adfe478c9ab
suekim999 <suekim999@naver.com>
2023-10-02 21:23:48 +0900

    [Chore] FeedViewController - viewWillAppear할 때마다 서버로부터 fetch해오도록 수정함

--- SoSoHappy/Sources/Presentation/Feed/ViewController/FeedViewController.swift
+++ SoSoHappy/Sources/Presentation/Feed/ViewController/FeedViewController.swift
@@ -15,31 +15,20 @@ import RxCocoa
 /*
  리팩토링
  1. 버튼을 여러번 클릭했을 때 API 중복 호출을 막아주는 조치 (하트, 오늘, 전체 버튼) - throttle, debouce
- 2. refresh control 한번 더 확인해보기 - 좀 모양새가 이상함
- 
  3. AlertReactor 프로젝트처럼 Reactor의 feeds를 [FeedCellReactor]로 해줘서 따로 Reactor 인스턴스를 만들지 않고
  cell.reactor = reactor 바로 주입 가능 (어떤 방법이 더 적합할지 고민해보기)
 
  */
 
-/*
- 1. refreshcontrol이 navigation title이 large로 해서 그런지 모양새가 이상하게 나와서 코드를 일단 수정해놓음
- */
-
-// MARK: - cell에서 feed를 받는게 좋을지 아니면 itemSelected로 feed를 받는게 좋을지 고민해보고 정하기
-
-// delegate 프로퍼티에 weak 키워드를 붙이려면 AnyObject를 써야 했음
-// weak의 용도와 AnyObject가 무엇인지 좀 찾아보기
-protocol FeedViewControllerDelegate: AnyObject {
+protocol FeedViewControllerDelegate: AnyObject { // only adopted by class
     func showdDetails(feed: FeedTemp) // feed 넘겨주기만 하면 됨 (따로 서버 통신 필요 없음)
     func showOwner(ownerNickName: String) // 조회대상 닉네임이 필요 ('특정 유저 피드 조회'는 서버통신 필요)
 }
 
-
 final class FeedViewController: UIViewController {
     // MARK: - Properties
     var disposeBag = DisposeBag()
-    weak var delegate: FeedViewControllerDelegate?
+    weak var delegate: FeedViewControllerDelegate?  // weak - must have a reference type (not value)
     
     // MARK: - UI Components
     private lazy var feedHeaderView = FeedHeaderView()
@@ -60,10 +49,12 @@ final class FeedViewController: UIViewController {
         super.viewDidLoad()
         print("FeedViewController viewDidLoad ---------------")
         setup()
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        print("FeedViewController viewWillAppear ---------------")
         let contentOffset = self.tableView.contentOffset
         handleTableViewContentOffset(contentOffset)
     }
@@ -88,16 +79,6 @@ final class FeedViewController: UIViewController {
 //MARK: - Set Navigation & Add Subviews & Constraints
 extension FeedViewController {
     private func setup() {
-//        configureNavigation()
-        setLayout()
-    }
-    
-    func configureNavigation() {
-//        self.navigationItem.title = "소피들의 소소해피"
-//        self.navigationController?.navigationBar.prefersLargeTitles = true
-    }
-    
-    private func setLayout() {
         view.addSubview(tableView)
         tableView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
@@ -110,7 +91,7 @@ extension FeedViewController: View {
     // MARK: bind
     func bind(reactor: FeedViewReactor) {
         // MARK: Action (View -> Reactor) 인풋
-        self.rx.viewDidLoad
+        self.rx.viewWillAppear
             .map { Reactor.Action.fetchFeeds(.today) } // default today
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
@@ -155,6 +136,7 @@ extension FeedViewController: View {
             .bind(to: tableView.rx.items(cellIdentifier: FeedCell.cellIdentifier, cellType: FeedCell.self)) { [weak self] (row,  feed, cell) in
                 guard let self = self else { return }
                 configureCell(cell, with: feed)
+                print("tableView cell 세팅중")
             }
             .disposed(by: disposeBag)
         
@@ -163,12 +145,17 @@ extension FeedViewController: View {
             .map { $0.sortOption }
             .subscribe(onNext: { [weak self] sortOption in
                 guard let self = self else { return }
+                print("sortOption")
                 feedHeaderView.updateButtonState(sortOption)
             })
             .disposed(by: disposeBag)
         
         reactor.state
-            .map { $0.isRefreshing }
+            .map {
+                print("isRefreshing")
+                return $0.isRefreshing
+                
+            }
             .distinctUntilChanged()
             .bind(to: self.refreshControl.rx.isRefreshing)
             .disposed(by: disposeBag)
@@ -177,14 +164,14 @@ extension FeedViewController: View {
             .compactMap { $0.selectedFeed }
             .subscribe(onNext: { [weak self] feed in
                 guard let self = self else { return }
-                print("여기까지 진행완료")
+                print("selectedFeed : \(feed)")
 //                print("feed: \(feed), type: \(type(of: feed))")
                 self.delegate?.showdDetails(feed: feed)
             })
             .disposed(by: disposeBag)
     }
     
-    func configureCell(_ cell: FeedCell, with feed: FeedTemp) {
+    private func configureCell(_ cell: FeedCell, with feed: FeedTemp) {
         // MARK: FeedReactor에 feed 넣어주는 방법1
 //        let initialState = FeedReactor.State(feed: feed)
 //        let cellReactor = FeedReactor(state: initialState)
