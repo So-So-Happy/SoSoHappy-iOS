e1c5e4ac3fb9bd9dd22274426ef8014b1759c2f1
suekim999 <suekim999@naver.com>
2023-10-14 17:26:19 +0900

    [Chore]  FeedCoordinator 주석 수정 (#50)

--- SoSoHappy/Sources/Presentation/Feed/Coordinator/Protocol/FeedCoordinator.swift
+++ SoSoHappy/Sources/Presentation/Feed/Coordinator/Protocol/FeedCoordinator.swift
@@ -9,20 +9,21 @@ import UIKit
 
 /*
  궁금한 점
- 1. FeedCoordinatorInterface 용도
- 2. FeedCoordinator type을 .feed로 해야하는게 맞지?
+ 1. FeedCoordinator type을 .feed로 해야하는게 맞지?
  */
 
 /*
  1. 중복되는 코드 리팩토링 - didSelectCell 자체가 많이 중복됨
  2. 데이터 넘겨주는 거 작성하기 (Reactor에 API request 해야할 때 보내야하는것들이 잘 챙겨져 있는지 확인하고 작성하기)
  */
-// MARK:
+
+// MARK: - FeedCoordinatorInterface
 protocol FeedCoordinatorInterface: Coordinator {
     func showdDetails(feed: FeedTemp) // feed 넘겨주기만 하면 됨 (따로 서버 통신 필요 없음)
     func showOwner(ownerNickName: String) // 조회대상 닉네임이 필요 ('특정 유저 피드 조회'는 서버통신 필요)
 }
 
+// MARK: - FeedCoordinator
 final class FeedCoordinator: FeedCoordinatorInterface {
     var type: CoordinatorType { .feed }
     var parentCoordinator: Coordinator?
