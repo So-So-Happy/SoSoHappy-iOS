eeb6dc6a6be08e02b60e8173ca61283ef09d74d1
suekim999 <suekim999@naver.com>
2023-10-21 13:12:22 +0900

    [Add] Feed 관련 Response 파일 toDomain 메서드 구현 및 불필요한 코드 삭제 (#53)

---
+++ SoSoHappy/Sources/Network/Models/DTO/Responses/FindIntroductionResponse.swift
@@ -0,0 +1,12 @@
+//
+//  FindIntroductionResponse.swift
+//  SoSoHappy
+//
+//  Created by Sue on 10/20/23.
+//
+
+import Foundation
+
+struct FindIntroductionResponse: Decodable {
+    let introduction: String
+}
