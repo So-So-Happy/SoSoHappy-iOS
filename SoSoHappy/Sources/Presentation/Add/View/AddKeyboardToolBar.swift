42a588b34c3b9ff72c4c346cede66a1cf590e033
suekim999 <suekim999@naver.com>
2023-10-21 13:31:02 +0900

    [Add] AddKeyboardToolBar - AddStep3ViewController에서 사용할 toolabar 파일 분리 (#56)

---
+++ SoSoHappy/Sources/Presentation/Add/View/AddKeyboardToolBar.swift
@@ -0,0 +1,42 @@
+//
+//  AddKeyboardToolBar.swift
+//  SoSoHappy
+//
+//  Created by Sue on 10/17/23.
+//
+
+import UIKit
+import SnapKit
+
+// lock , lock.open
+// photo
+// chevron.down
+
+final class AddKeyboardToolBar: UIToolbar {
+    // MARK: - UI Components
+    lazy var photoBarButton = UIBarButtonItem(image: UIImage(systemName: "photo"), style: .plain, target: nil, action: nil)
+    lazy var lockBarButton = UIBarButtonItem(image: UIImage(systemName: "lock"), style: .plain, target: nil, action: nil)
+    private lazy var flexibleSpace = UIBarButtonItem(systemItem: .flexibleSpace)
+    lazy var keyboardDownBarButton = UIBarButtonItem(image: UIImage(systemName: "chevron.down"), style: .plain, target: nil, action: nil)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.items = [photoBarButton, lockBarButton, flexibleSpace, keyboardDownBarButton]
+        self.sizeToFit()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - setting functions
+extension AddKeyboardToolBar {
+    // MARK: isPrivate에 따라 lockBarButton 이미지 세팅
+    func setPrivateTo(_ isPrivate: Bool) {
+        let systemName = isPrivate ? "lock" : "lock.open"
+        let image = UIImage(systemName: systemName)
+        lockBarButton.image = image
+    }
+}
