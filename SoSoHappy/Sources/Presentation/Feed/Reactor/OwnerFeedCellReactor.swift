2fa68428f8cb1365131415742c8dd2228ac19b60
suekim999 <suekim999@naver.com>
2023-10-28 16:04:28 +0900

    [Chore] OwnerFeedCellReactor 필요없는 것 같아서 주석 처리. 추후에 리팩토링하면서 지울 예정 (#53)

--- SoSoHappy/Sources/Presentation/Feed/Reactor/OwnerFeedCellReactor.swift
+++ SoSoHappy/Sources/Presentation/Feed/Reactor/OwnerFeedCellReactor.swift
@@ -7,47 +7,47 @@
 
 import ReactorKit
 
-class OwnerFeedCellReactor: Reactor {
-    enum Action {
-        case toggleLike
-    }
-    
-    enum Mutation {
-        case setFeed(FeedTemp)
-    }
-    
-    struct State {
-        var feed: FeedTemp?
-    }
-    
-    let initialState: State
-    
-    init(state: State) {
-        self.initialState = state
-    }
-    
-    func mutate(action: Action) -> Observable<Mutation> {
-        switch action {
-        case .toggleLike:
-            // Toggle the like state and emit a Mutation to update the UI
-            guard var updatedFeed = currentState.feed else { fatalError() }
-            updatedFeed.isLike = !updatedFeed.isLike
-            // update된 피드 서버에도 알려주기
-            return Observable.just(.setFeed(updatedFeed))
-        }
-    }
-    
-    func reduce(state: State, mutation: Mutation) -> State {
-        var newState = state
-        switch mutation {
-//        case let .setLike(like):
-//            newState.isLike = like
-        case .setFeed(let feed):
-            newState.feed = feed
-        }
-        
-        return newState
-    }
-}
-
-
+//class OwnerFeedCellReactor: Reactor {
+//    enum Action {
+//        case toggleLike
+//    }
+//    
+//    enum Mutation {
+//        case setFeed(FeedTemp)
+//    }
+//    
+//    struct State {
+//        var feed: FeedTemp?
+//    }
+//    
+//    let initialState: State
+//    
+//    init(state: State) {
+//        self.initialState = state
+//    }
+//    
+//    func mutate(action: Action) -> Observable<Mutation> {
+//        switch action {
+//        case .toggleLike:
+//            // Toggle the like state and emit a Mutation to update the UI
+//            guard var updatedFeed = currentState.feed else { fatalError() }
+//            updatedFeed.isLike = !updatedFeed.isLike
+//            // update된 피드 서버에도 알려주기
+//            return Observable.just(.setFeed(updatedFeed))
+//        }
+//    }
+//    
+//    func reduce(state: State, mutation: Mutation) -> State {
+//        var newState = state
+//        switch mutation {
+////        case let .setLike(like):
+////            newState.isLike = like
+//        case .setFeed(let feed):
+//            newState.feed = feed
+//        }
+//        
+//        return newState
+//    }
+//}
+//
+//
