0286f53ebeaf18650d52137c5107cd92d4137938
suekim999 <suekim999@naver.com>
2023-10-28 16:02:51 +0900

    [Add] FeedViewController - indicator view 나타날 수 있도록 NVActivityIndicatorView 추가 (#53)

--- SoSoHappy/Sources/Presentation/Feed/ViewController/FeedViewController.swift
+++ SoSoHappy/Sources/Presentation/Feed/ViewController/FeedViewController.swift
@@ -11,17 +11,21 @@ import Then
 import ReactorKit
 import RxSwift
 import RxCocoa
+import NVActivityIndicatorView
 
 /*
  리팩토링
- 1. 버튼을 여러번 클릭했을 때 API 중복 호출을 막아주는 조치 (하트, 오늘, 전체 버튼) - throttle, debouce
+ 1. 버튼을 여러번 클릭했을 때 API 중복 호출을 막아주는 조치 (하트, 오늘, 전체 버튼) - throttle, debouce 적용해주기
  3. AlertReactor 프로젝트처럼 Reactor의 feeds를 [FeedCellReactor]로 해줘서 따로 Reactor 인스턴스를 만들지 않고
  cell.reactor = reactor 바로 주입 가능 (어떤 방법이 더 적합할지 고민해보기)
  */
 
 /*
- 1. refresh 할 때 Layout 에러 해결해주기
- 2. '등록된 뷰가 없습니다'를 좀 더 자연스럽게 보여줄 수 있으면 좋을 것 같음
+ 1. 예외 상황
+ - '등록된 뷰가 없습니다' -> userFeeds가 비어있을 때
+ - 403일 때는 애초에 토큰을 재발급해서
+ - 500 서버오류
+ - 네트워크 연결이 안되어 있습니다.
  */
 
 final class FeedViewController: UIViewController {
@@ -37,6 +41,8 @@ final class FeedViewController: UIViewController {
         topOffset: 300
     )
     
+    // MARK: 로딩 뷰 잘 넣어주기
+    private lazy var activityIndicatorView = NVActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 27, height: 27), type: .circleStrokeSpin, color: .black, padding: 0)
     
     private lazy var tableView = UITableView().then {
         $0.register(FeedCell.self, forCellReuseIdentifier: FeedCell.cellIdentifier)
@@ -84,8 +90,15 @@ final class FeedViewController: UIViewController {
 extension FeedViewController {
     private func setLayout() {
         view.addSubview(tableView)
+        tableView.addSubview(activityIndicatorView)
+        
         tableView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
+        }
+        
+        activityIndicatorView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(feedHeaderView.snp.bottom).offset(50)
         }
     }
 }
@@ -105,13 +118,11 @@ extension FeedViewController: View {
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
         
-        // sortTodayButton, feedHeaderView에 연타방지 연산 필요
-        // debouce :
-        // throttle :
+        // sortTodayButton, feedHeaderView에 연타방지 연산 필요 (thorttle, debounce)
         feedHeaderView.sortTodayButton.rx.tap
 //            .debounce(.milliseconds(600), scheduler: MainScheduler.instance)
             .map {
-                print("오늘 누름")
+                print("FeedVC - 오늘 누름")
                 return Reactor.Action.fetchFeeds(.today)
             }
             .bind(to: reactor.action)
@@ -119,7 +130,7 @@ extension FeedViewController: View {
         
         feedHeaderView.sortTotalButton.rx.tap
             .map {
-                print("전체 누름")
+                print("FeedVC - 전체 누름")
                 return Reactor.Action.fetchFeeds(.total)
             }
             .bind(to: reactor.action)
@@ -139,48 +150,26 @@ extension FeedViewController: View {
             })
             .disposed(by: disposeBag)
 
-        // MARK: State (Reactor -> State) 아웃풋
+        // MARK: - State (Reactor -> State) 아웃풋
+        // MARK: tableView에 userFeeds 데이터 연결
         reactor.state
             .skip(1)
-            .map {
-                print("reactor.state - feeds : \($0.userFeeds)")
+            .compactMap {
+                print("FeedVC - reactor.state - feeds : \($0.userFeeds)")
                 return $0.userFeeds
             }
             .bind(to: tableView.rx.items(cellIdentifier: FeedCell.cellIdentifier, cellType: FeedCell.self)) { [weak self] (row,  userFeed, cell) in
-                print("tableView cell 세팅중!! - userFeed : \(userFeed)")
+                print(" FeedVC - tableView cell 세팅중!! - userFeed : \(userFeed)")
                 guard let self = self else { return }
                 configureCell(cell, with: userFeed)
             }
             .disposed(by: disposeBag)
         
-        
-        reactor.state
-            .skip(1)
-            .map {
-                print("Feeds updated 되기 전 map : \($0.userFeeds)")
-                return $0.userFeeds
-            }
-            .distinctUntilChanged()
-            .bind(onNext: { [weak self] userFeeds in
-                guard let self = self else { return }
-                print("@@ @@ @@ Feeds updated: \(userFeeds)")
-                
-                if userFeeds.isEmpty {
-                    print("없다")
-                    addEmptyView()
-                } else {
-                    print("있다")
-                    removeEmptyView()
-                }
-                
-//                self.noFeedLabel.isHidden = !feeds.isEmpty
-            })
-            .disposed(by: disposeBag)
-        
+        // MARK: 정렬 기준에 따라서 button 색상 지정
         reactor.state
             .skip(1)
             .compactMap {
-                print("reactor.state - sortOption : \($0.sortOption)")
+                print("FeedVC - reactor.state - sortOption : \($0.sortOption)")
                 return $0.sortOption
             }
             .bind(onNext: { [weak self] sortOption in
@@ -190,10 +179,30 @@ extension FeedViewController: View {
             })
             .disposed(by: disposeBag)
         
+        
+        // MARK: 서버로부터 데이터를 받아오는 동안 loading view
+        reactor.state
+            .compactMap { $0.isLoading }
+            .distinctUntilChanged()
+            .bind(onNext: { [weak self] isLoading in
+                guard let self = self else { return }
+                print("FeedVC - isLoading: \(isLoading)")
+                if isLoading {
+                    print("FeedVC - animating")
+                    activityIndicatorView.startAnimating()
+                } else {
+                    print("FeedVC - animating stop")
+                    activityIndicatorView.stopAnimating()
+                }
+            })
+            .disposed(by: disposeBag)
+        
+       
+        // MARK: 새로고침했을 때
         reactor.state
             .skip(1)
             .map {
-                print("reactor.state - isRefreshing : \($0.isRefreshing)")
+                print("FeedVC - reactor.state - isRefreshing : \($0.isRefreshing)")
                 return $0.isRefreshing
                 
             }
@@ -201,16 +210,22 @@ extension FeedViewController: View {
             .debug()
             .bind(to: self.refreshControl.rx.isRefreshing)
             .disposed(by: disposeBag)
+
+
         
+        // MARK: cell 선택 view 이동
         reactor.state
             .compactMap { $0.selectedFeed }
             .subscribe(onNext: { [weak self] userFeed in
                 guard let self = self else { return }
-                print("selectedFeed!! : \(userFeed) ")
+                print("FeedVC - selectedFeed!! : \(userFeed) ")
 //                print("feed: \(feed), type: \(type(of: feed))")
                 self.coordinator?.showdDetails(userFeed: userFeed)
             })
             .disposed(by: disposeBag)
+        
+        // MARK: 올아온 게시물이 없습니다.
+       
     }
 }
 
@@ -218,11 +233,9 @@ extension FeedViewController: View {
 extension FeedViewController {
     private func configureCell(_ cell: FeedCell, with userFeed: UserFeed) {
         // MARK: FeedReactor에 feed 넣어주는 방법1
-//        let initialState = FeedReactor.State(feed: feed)
-//        let cellReactor = FeedReactor(state: initialState)
         
         // MARK: FeedReactor에 feed 넣어주는 방법2
-        let cellReactor = FeedReactor(userFeed: userFeed, feedRepository: FeedRepository())
+        let cellReactor = FeedReactor(userFeed: userFeed, feedRepository: FeedRepository(), userRepository: UserRepository())
         cell.reactor = cellReactor
         
         // - 여기에 코드를 작성한 이유
@@ -263,3 +276,30 @@ extension FeedViewController {
         exceptionView.removeFromSuperview()
     }
 }
+
+
+/*
+ reactor.state
+     .skip(1)
+     .map {
+         print("Feeds updated 되기 전 map : \($0.userFeeds)")
+         return $0.userFeeds
+     }
+     .distinctUntilChanged()
+     .bind(onNext: { [weak self] userFeeds in
+         guard let self = self else { return }
+         print("@@ @@ @@ Feeds updated: \(userFeeds)")
+         
+         if userFeeds.isEmpty {
+             print("없다")
+             addEmptyView()
+         } else {
+             print("있다")
+             removeEmptyView()
+         }
+         
+//                self.noFeedLabel.isHidden = !feeds.isEmpty
+     })
+     .disposed(by: disposeBag)
+ 
+ */
