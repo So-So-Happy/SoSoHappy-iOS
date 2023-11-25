525a23311acef81506350d88ce78d887aa28a7bc
Minju Park <mlnjv016@gmail.com>
2023-11-26 01:37:20 +0900

    [Chore] 스켈레톤 뷰 코드 변경 (#76)

--- SoSoHappy/Sources/Presentation/MyPage/View/ProfileVIew.swift
+++ SoSoHappy/Sources/Presentation/MyPage/View/ProfileVIew.swift
@@ -16,20 +16,30 @@ final class ProfileView: UIView {
     lazy var nickNameLabel = UILabel().then {
         $0.textAlignment = .center
         $0.font = .systemFont(ofSize: 28, weight: .bold)
-        $0.layer.masksToBounds = true
-        $0.layer.cornerRadius = 5
     }
     lazy var emailLabel = UILabel().then {
         $0.font = .systemFont(ofSize: 12, weight: .light)
         $0.textColor = .gray
-        $0.layer.masksToBounds = true
-        $0.layer.cornerRadius = 3
     }
     lazy var introLabel = UILabel().then {
         $0.font = .systemFont(ofSize: 16, weight: .regular)
         $0.textColor = .darkGray
         $0.numberOfLines = 0
-        $0.layer.masksToBounds = true
+        $0.textAlignment = .center
+    }
+    
+    lazy var skeletonNicknameView = UIView().then {
+        $0.backgroundColor = UIColor(named: "skeleton3")
+        $0.layer.cornerRadius = 5
+    }
+    
+    lazy var skeletonEmailView = UIView().then {
+        $0.backgroundColor = UIColor(named: "skeleton1")
+        $0.layer.cornerRadius = 3
+    }
+    
+    lazy var skeletonIntroView = UIView().then {
+        $0.backgroundColor = UIColor(named: "skeleton2")
         $0.layer.cornerRadius = 4
     }
 
@@ -48,6 +58,7 @@ final class ProfileView: UIView {
 extension ProfileView {
     func setProfileView() {
         addSubviews(profileImage, nickNameLabel, emailLabel, introLabel)
+        addSubviews(skeletonNicknameView, skeletonEmailView, skeletonIntroView)
         
         profileImage.snp.makeConstraints { make in
             make.top.equalTo(self.safeAreaLayoutGuide)
@@ -67,8 +78,26 @@ extension ProfileView {
         
         introLabel.snp.makeConstraints { make in
             make.top.equalTo(emailLabel.snp.bottom).offset(13)
-            make.centerX.equalToSuperview()
+            make.horizontalEdges.equalToSuperview()
             make.bottom.equalToSuperview()
+        }
+        
+        skeletonNicknameView.snp.makeConstraints { make in
+            make.center.equalTo(nickNameLabel)
+            make.width.equalTo(150)
+            make.height.equalTo(30)
+        }
+        
+        skeletonEmailView.snp.makeConstraints { make in
+            make.center.equalTo(emailLabel)
+            make.width.equalTo(130)
+            make.height.equalTo(16)
+        }
+        
+        skeletonIntroView.snp.makeConstraints { make in
+            make.center.equalTo(introLabel)
+            make.width.equalTo(200)
+            make.height.equalTo(22)
         }
     }
 }
