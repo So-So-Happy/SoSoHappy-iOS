03dfa1144a0cd0094d2f0c15640d2289f6c822db
Minju Park <mlnjv016@gmail.com>
2023-11-29 20:28:19 +0900

    [Feat] 로그인 API 디바이스 토큰 추가

--- SoSoHappy/Application/AppDelegate.swift
+++ SoSoHappy/Application/AppDelegate.swift
@@ -50,6 +50,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        // MARK: Messaging Delegate
         Messaging.messaging().delegate = self
         
+        UIBarButtonItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor(named: "AccentColor")!, NSAttributedString.Key.font: UIFont.customFont(size: 16, weight: .medium)], for: UIControl.State.normal)
+        
         return true
     }
     
@@ -125,18 +127,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 // MARK: - MessagingDelegate
 extension AppDelegate: MessagingDelegate {
     func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-      print("Firebase registration token: \(String(describing: fcmToken))")
-
-      let dataDict: [String: String] = ["token": fcmToken ?? ""]
-      NotificationCenter.default.post(
-        name: Notification.Name("FCMToken"),
-        object: nil,
-        userInfo: dataDict
-      )
-      // TODO: If necessary send token to application server.
-      // Note: This callback is fired at each app startup and whenever a new token is generated.
+        print("Firebase registration token: \(String(describing: fcmToken))")
+        UserDefaults.standard.setValue(fcmToken, forKey: "fcmToken")
+        
+        let dataDict: [String: String] = ["token": fcmToken ?? ""]
+        NotificationCenter.default.post(
+            name: Notification.Name("FCMToken"),
+            object: nil,
+            userInfo: dataDict
+        )
+        // TODO: If necessary send token to application server.
+        // Note: This callback is fired at each app startup and whenever a new token is generated.
     }
-
+    
 }
 
 
