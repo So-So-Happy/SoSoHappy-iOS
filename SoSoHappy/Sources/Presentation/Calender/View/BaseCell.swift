ddbf929f18c2b522ef09e3f747c8dac6a78a1c0d
suekim999 <suekim999@naver.com>
2023-12-19 21:25:31 +0900

    [Chore] BaseCell 주석 제거

--- SoSoHappy/Sources/Presentation/Calender/View/BaseCell.swift
+++ SoSoHappy/Sources/Presentation/Calender/View/BaseCell.swift
@@ -11,11 +11,7 @@ import ImageSlideshow
 import Then
 import ReactorKit
 
-/*
- 1. 한번 더 리팩토링하면 좋을 듯 - imageslideshow height
- */
 
-// MARK: - BaseCell 리팩토링 필요
 class BaseCell: UITableViewCell {
     // MARK: - Properties
     var disposeBag = DisposeBag()
@@ -32,13 +28,10 @@ class BaseCell: UITableViewCell {
         $0.layer.cornerRadius = 16
     }
     
-    // 날씨 이미지 + 작성 날짜
     lazy var weatherDateStackView = WeatherDateStackView()
     
-    // 행복 + 카테고리
     private lazy var categoryStackView = CategoryStackView()
     
-    // 피드 작성 글
     private lazy var contentLabel = UILabel().then {
         $0.textAlignment = .left
         $0.font = UIFont.customFont(size: 15, weight: .medium)
@@ -46,7 +39,6 @@ class BaseCell: UITableViewCell {
         $0.numberOfLines = 4
     }
     
-    // 피드 이미지
     lazy var imageSlideView = ImageSlideView()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -65,20 +57,15 @@ class BaseCell: UITableViewCell {
         categoryStackView.addImageViews(images: feed.happinessAndCategoryArray, imageSize: 45)
         contentLabel.text = feed.text
         
-        // TODO: 이 부분에서 레이아웃 에러가 나는 것 같아서 다시 한번 봐야 함
-        if feed.imageIdList.isEmpty { //image가 없다면
-            print("BaseCell 사진 없음(X)")
-            imageSlideViewHeightConstraint?.isActive = false // 제약조건 비활성화
+        if feed.imageIdList.isEmpty {
+            imageSlideViewHeightConstraint?.isActive = false
             
-        } else { // image가 있으면
-            print("BaseCell 사진 있음(O)")
-            imageSlideViewHeightConstraint?.isActive = true // 제약조건 활성화
+        } else {
+            imageSlideViewHeightConstraint?.isActive = true
             imageSlideView.setImages(ids: feed.imageIdList)
         }
                 
-        // 속성을 초기화
         func prepareForReuse() {
-            print("BaseCell - prepareForReuse")
             super.prepareForReuse()
             disposeBag = DisposeBag()
         }
@@ -94,7 +81,7 @@ extension BaseCell {
     }
     
     private func setCellAttributes() {
-        backgroundColor = .clear // tableView의 backgroundColor가 보이도록 cell은 .clear
+        backgroundColor = .clear
         selectionStyle = .none
     }
     
@@ -107,8 +94,6 @@ extension BaseCell {
     }
     
     private func setConstraints() {
-        print("BaseCell setConstraints")
-        // 이 코드가 없어도 잘 동작하긴 함
         cellBackgroundView.snp.makeConstraints { make in
             make.edges.equalToSuperview().inset(UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16))
             make.bottom.equalTo(imageSlideView.snp.bottom).offset(40)
@@ -137,11 +122,8 @@ extension BaseCell {
             make.horizontalEdges.equalTo(cellBackgroundView).inset(15)
         }
         
-        // Set up the height constraint but do not activate it
         imageSlideViewHeightConstraint = imageSlideView.heightAnchor.constraint(equalToConstant: 200)
-        imageSlideViewHeightConstraint?.priority = .defaultLow // 상대적으로 낮은 중요도
-        // .defaultLow한 이유
-        // 이미지가 없는 경우에는 이 제약조건을 무시하고 0으로 만들 수 있도록 하기 위함
+        imageSlideViewHeightConstraint?.priority = .defaultLow
     }
 }
 
